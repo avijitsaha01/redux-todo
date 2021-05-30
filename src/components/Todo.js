ff38e5d2edb9ef4d065f9caca874dd788c39ab94
avijitsaha01 <avijitsahashovo2016@gmail.com>
2021-05-30 17:10:10 +0600

    update

---
+++ src/components/Todo.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import "./Todo.css";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, deleteTodo, removeTodo } from "../actions/index";
+
+const Todo = () => {
+
+    const [inputData, setinputData] = useState('');
+    const list = useSelector((state) => state.todoReducer.list);
+    const dispatch = useDispatch();
+
+    return (
+        <div className="main-div">
+            <div className="child-div">
+                <figure>
+                    <figcaption>Add your work-list here</figcaption>
+                </figure>
+                <div className="addItems">
+                    <input type="text" placeholder="Add Item..."
+                        value={inputData}
+                        onChange={(event) => setinputData(event.target.value)}
+                    />
+                    <i className="fa fa-plus add-btn" onClick={() => dispatch(addTodo(inputData), setinputData(''))}></i>
+                </div>
+
+                <div className="showItems">
+                    {
+                        list.map((element) => {
+                            return (
+                                <div className="eachItem" key={element.id}>
+                                    <h3>{element.data}</h3>
+                                    <div className="todo-btn">
+                                        <i className="far fa-trash-alt add-btn" title="Delete Item" onClick={() => dispatch(deleteTodo(element.id))}></i>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+
+
+                <div className="">
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Todo;
